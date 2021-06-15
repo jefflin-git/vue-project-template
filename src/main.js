9b2e5f02533654c0bab1bdf5dd1c19daac52d637
jeff lin <linjef0711@gmail.com>
2021-06-15 18:41:53 +0800

    feat : add jQuery $

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 讓瀏覽器的全域環境可以使用到 $
+// import jQuery from 'jquery'
+// window.$ = window.jQuery = jQuery
 
 Vue.config.productionTip = false
 
